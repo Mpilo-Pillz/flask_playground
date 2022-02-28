6fb8b4412c189c35e7fb245e0db27b07ede04bd2
mpilopillz <mpilo.dlamini@yahoo.com>
2022-02-28 13:33:22 +0200

    adding product schema

--- flask-rest-api/app.py
+++ flask-rest-api/app.py
@@ -1,3 +1,4 @@
+from email.policy import strict
 from enum import unique
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -27,7 +28,23 @@ class Product(db.Model):
     name = db.Column(db.String(100), unique=True)
     description = db.Column(db.String(200))
     price = db.Column(db.Float)
-    qty = db.Column(db.Integer)   
+    qty = db.Column(db.Integer) 
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
+
+# Product Schema
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'qty' )
+
+#Init Schema
+product_schema = ProductSchema(strict=True)
+products_schema = ProductSchema(many=True, strict=True)
+
 # Run Server
 if __name__ == '__main__':
     app.run(debug=True)
