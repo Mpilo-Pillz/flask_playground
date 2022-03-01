9329067ac1f6da24beee0ab565982b98a8128460
mpilopillz <mpilo.dlamini@yahoo.com>
2022-03-01 13:03:02 +0200

    updating 1

--- flask-rest-api/app.py
+++ flask-rest-api/app.py
@@ -1,5 +1,6 @@
 from crypt import methods
 from enum import unique
+from math import prod
 from unittest import result
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -72,6 +73,25 @@ def get_products():
 def get_product(id):
     product = Product.query.get(id)
     return product_schema.jsonify(product)
+
+@app.route('/product/<id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get(id)
+
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json['qty']
+
+    product.name = name
+    product.description = description
+    product.price = price
+    product.qty = qty
+
+    
+    db.session.commit()
+
+    return product_schema.jsonify(product)    
 
 # Run Server
 if __name__ == '__main__':
